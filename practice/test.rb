519f2aca1caf2b8d765d8356d7224c80ddc8d79d
Runa Honzaki <runa.honzaki@rizapgroup.com>
2026-05-21 12:10:04 +0900

    chapter3の修正

---
+++ practice/test.rb
@@ -0,0 +1,16 @@
+# chapter2
+weight=60
+height=1.7
+BMI=weight/height**2
+puts "体重#{weight}kg、身長#{height}のBMIは#{BMI}です"
+
+# chapter3
+count = 5
+if count < 4
+    puts "もう少し頑張りましょう"
+elsif 4 <= count && count < 8
+    puts "よく頑張りました"
+else
+    puts "最高です"
+end
+
